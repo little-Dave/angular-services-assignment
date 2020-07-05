4b3c37b82a70eee7c4c848e20bdf09af89fc7687
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-07-05 10:48:11 -0600

    utilize UsersService from both active-users and inactiveUsers component.ts files
    migrate logic from app.component to users.service;
    import users.service into activeUsers and inactiveUsers component.ts files;
    access methods to update active and inactive users arrays through users.service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,13 +18,4 @@ export class AppComponent implements OnInit{
     this.inactiveUsers = this.usersService.inactiveUsers;
   }
   
-  onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
-  }
-
-  onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
-  }
 }
