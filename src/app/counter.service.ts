112b3f17298e54f98ca012058441b8d7644f5a24
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-07-05 11:08:12 -0600

    write logic in and utilize logic from counter.service
    create counters that track number of conversions from active to inactive and vice versa;
    increment and log these counters when respective UsersService methods are called from active and/or inactive-users.component(s)

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -1,3 +1,6 @@
 export class CounterService {
-  
+  changeCounters = {
+    activeToInactive: 0,
+    inactiveToActive: 0
+  };
 }
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,16 +1,27 @@
+import { Injectable } from '@angular/core';
+
+import { CounterService } from './counter.service';
+
+@Injectable()
 export class UsersService {
 
   activeUsers = ['Max', 'Anna'];
   inactiveUsers = ['Chris', 'Manu'];
 
+  constructor(private counterService: CounterService) {}
+  
   setToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
+    this.counterService.changeCounters.activeToInactive++;
+    console.log('set to inactive: ' + this.counterService.changeCounters.activeToInactive);
   }
 
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+    this.counterService.changeCounters.inactiveToActive++;
+    console.log('set to active: ' + this.counterService.changeCounters.inactiveToActive);
   }
 
 }
